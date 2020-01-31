75a7cd4dec2140cadc69370e53bfca01de6bba4a
Rikux3 <d.drankov@gmail.com>
2020-01-31 21:42:19 +0100

    Support for mabf files

--- SabfExtract/Program.cs
+++ SabfExtract/Program.cs
@@ -8,6 +8,7 @@ namespace SabfExtract
     class Program
     {
         private static readonly byte[] SABFMAGIC = { 0x73, 0x61, 0x62, 0x66 };
+        private static readonly byte[] MABFMAGIC = { 0x6d, 0x61, 0x62, 0x66 };
         static void Main(string[] args)
         {
             if (args.Length > 0)
@@ -34,25 +35,59 @@ namespace SabfExtract
                 Console.WriteLine($"Opening file {fileName}");
                 var fileIn = File.ReadAllBytes(fileName);
                 var assetData = fileIn.Take(fileIn.Length - 4).ToArray();
-                var sabfInstances = FindPattern(assetData, SABFMAGIC).ToList();
-                if (sabfInstances.Count > 1)
+
+                Console.WriteLine("Scanning for sabf header...");
+
+                var sabfInstances = HasPattern(assetData, SABFMAGIC);
+                if (sabfInstances.Item1)
                 {
-                    Console.WriteLine("More than one instance of sabf found! Aborting...");
-                    return;
+                    if (sabfInstances.Item2.Count > 1)
+                    {
+                        Console.WriteLine("More than one instance of sabf found!");
+                    }
+                    else
+                    {
+                        var outName = $"{Path.GetFileName(fileName)}.sab";
+                        Console.WriteLine($"Saving new file {outName}");
+                        File.WriteAllBytes(outName, assetData.Skip(sabfInstances.Item2[0]).ToArray());
+                    }
                 }
-                else if (sabfInstances.Count == 0)
+                else
                 {
-                    Console.WriteLine("No instance of sabf found! Aborting...");
-                    return;
+                    Console.WriteLine("No sabf header found!");
                 }
-                var outName = $"{Path.GetFileName(fileName)}.sab";
-                Console.WriteLine($"Saving new file {outName}");
-                File.WriteAllBytes(outName, assetData.Skip(sabfInstances[0]).ToArray());
+
+                Console.WriteLine("Scanning for mabf header...");
+
+                var mabfInstances = HasPattern(assetData, MABFMAGIC);
+                if (mabfInstances.Item1)
+                {
+                    if (mabfInstances.Item2.Count > 1)
+                    {
+                        Console.WriteLine("More than one instance of mabf found!");
+                    }
+                    else
+                    {
+                        var outName = $"{Path.GetFileName(fileName)}.mab";
+                        Console.WriteLine($"Saving new file {outName}");
+                        File.WriteAllBytes(outName, assetData.Skip(mabfInstances.Item2[0]).ToArray());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No mabf header found!");
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        public static (bool, List<int>) HasPattern(byte[] source, byte[] pattern)
+        {
+            var entries = FindPattern(source, pattern).ToList();
+            return (entries.Count > 0) ? (true, entries) : (false, entries);
         }
 
         public static IEnumerable<int> FindPattern(byte[] source, byte[] pattern)
